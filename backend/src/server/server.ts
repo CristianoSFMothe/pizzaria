ce5b74f658b6c259e3bc40fe80b57b9a2e500193
Cristiano da Silva Ferreira <cristianodevsystem@gmail.com>
2026-01-14 22:08:40 -0300

    feat: create initial config serve

---
+++ backend/src/server/server.ts
@@ -0,0 +1,12 @@
+import cors from "cors";
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT! || 33333;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
